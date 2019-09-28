b18444fdfd3f87fb9d5a8b1a636cacb95295c802
Detrayeryu <55904509+Detrayeryu@users.noreply.github.com>
2019-09-28 11:15:17 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello cheese')
+let moment = require('moment')
+let _ = require('lodash')
+
+
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+let d  = new Date();
+
+console.log(d)
+
+let strings = "HELLO"git 
+
+console.log(_.lowerCase(string))
